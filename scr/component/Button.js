18a3110a07dd31743fe3c2309d675dd633b41256
Mala-Dhakad <dhakadmala0@gmail.com>
2024-12-28 11:43:03 +0530

    coinapp

--- scr/component/Button.js
+++ scr/component/Button.js
@@ -1,54 +1,67 @@
-import { View, Text, TouchableOpacity, StyleSheet,Image } from 'react-native'
+// import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native'
+// import React from 'react'
+
+// function Button({ title, onPress, backgroundColor, fontSize, fontWeight, secondText, textImage, titleone, imageSource }) {
+
+
+//   return (
+//     <View style={styles.viewStyle}>
+//       <TouchableOpacity activeOpacity={0.6}
+//         style={styles.mainConternar}
+//         onPress={onPress}>
+//         <View style={styles.flex}>
+//           <Image source={imageSource} style={styles.image} />
+//           <Text style={styles.text}>{title}</Text>
+//         </View>
+
+
+//       </TouchableOpacity>
+//     </View>
+//   )
+// }
+// export default Button
+// const styles = StyleSheet.create({
+//   mainConternar: {
+//     height: 55,
+//     width:314,
+//     borderRadius: 14,
+//     borderColor: "rgba(0, 0, 0, 0.25)",
+//     borderWidth: 1,
+//     paddingRight: 20,
+//     textAlign: 'center',
+//     backgroundColor:'white',
+//     justifyContent:'center',
+
+
+//   },
+//   viewStyle: {
+//     margin: 10,
+//   },
+//   text: {
+//     color: 'black',
+//   },
+//   flex: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     marginTop: 5,
+//   },
+//   image: {
+//     width: 25,
+//     height: 25,
+//     marginHorizontal: 15,
+//   },
+// })
+
+import { View, Text } from 'react-native'
 import React from 'react'
 
-function Button({ title, onPress }) {
-
-
+const Button = () => {
   return (
-    <TouchableOpacity activeOpacity={0.6}
-      style={styles.mainConternar}
-      onPress={onPress}>
-        <View style={styles.content}>
-         
-
-       
-      
-      <Text style={styles.text}>{title}</Text>
-      <Image source={require('../assets/For.png')}
-          style={styles.image}
-          />
-      </View>
-    </TouchableOpacity>
-
+    <View>
+      <Text>Button</Text>
+    </View>
   )
 }
-export default Button
-const styles = StyleSheet.create({
-  mainConternar: {
-    height: 45,
-    width: "100%",
-    borderRadius: 6,
-    borderColor: "rgba(0, 0, 0, 0.25)",
-    borderWidth: 1,
-   
-    justifyContent:'center',
-    paddingHorizontal: 10,
-    marginTop: 24,
-  },
-  content:{
-    flexDirection: 'row', 
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  image: {
-    width: 15,
-    height: 15,
-    
-  },
-  text: {
-    fontWeight: '600',
-    color: 'black',
-    fontSize: 14,
 
-  },
-})
+export default Button
